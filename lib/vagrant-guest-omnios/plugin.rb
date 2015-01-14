c68d72f34e50ff1e7b5b0165d1f79db1577efb45
Clinton Wolfe <clinton@omniti.com>
2015-01-13 21:36:48 -0500

    Adds insert/remove public SSH key capability

--- lib/vagrant-guest-omnios/plugin.rb
+++ lib/vagrant-guest-omnios/plugin.rb
@@ -31,6 +31,16 @@ module VagrantPlugins
         Cap::ChefInstall
       end
 
+      guest_capability(:omnios, :insert_public_key) do
+        require_relative "cap/insert_public_key"
+        Cap::InsertPublicKey
+      end
+
+      guest_capability(:omnios, :remove_public_key) do
+        require_relative "cap/remove_public_key"
+        Cap::RemovePublicKey
+      end
+
     end
   end
 end
